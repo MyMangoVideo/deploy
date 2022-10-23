51a8da3eb54bc3752839cbdf7ff315b139aa24a7
Mahesh <mahesh@Maheshs-MacBook-Pro.local>
2022-10-23 20:31:11 +0530

    remove extra stars

--- script.sh
+++ script.sh
@@ -20,8 +20,6 @@ warn() {
 
 warn "Step 1: Base setup update and upgrade the server."
 warn "****************************************************************"
-warn "****************************************************************"
-warn "****************************************************************"
 msg "moving into dir /home/ubuntu..."
 cd /home/ubuntu
 msg "running update and upgrade server..."
@@ -32,8 +30,6 @@ sudo apt upgrade -y
 msg "upgrade complete..."
 
 warn "Step 2: Installing packages nginx, wget and ruby"
-warn "****************************************************************"
-warn "****************************************************************"
 warn "****************************************************************"
 msg "installing nginx, wget and ruby..."
 ## install the nginx
@@ -46,8 +42,6 @@ msg "package installation done."
 
 warn "Step 3: Setting up nvm"
 warn "****************************************************************"
-warn "****************************************************************"
-warn "****************************************************************"
 msg "running nvm..."
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
@@ -59,16 +53,12 @@ msg "nvm installation done..."
 
 warn "Step 4: Installing nodejs 16.16.0"
 warn "****************************************************************"
-warn "****************************************************************"
-warn "****************************************************************"
 msg "installing nodejs v16.16.0"
 ## install node 16.16.0
 nvm install v16.16.0
 msg "node 16 installation done..."
 
 warn "Step 5: Installing pm2"
-warn "****************************************************************"
-warn "****************************************************************"
 warn "****************************************************************"
 msg "installing pm2"
 ## install pm2 globally
@@ -77,16 +67,12 @@ npm install pm2@latest -g
 
 warn "Step 6: Make sure the npm is of version 6.14.1"
 warn "****************************************************************"
-warn "****************************************************************"
-warn "****************************************************************"
 msg "setting npm to 6.14.1"
 ## revert npm version from latest to 6.14.1
 npm i -g npm@6.14.1
 msg "npm installation done..."
 
 warn "Step 7: Installing Code deploy agent"
-warn "****************************************************************"
-warn "****************************************************************"
 warn "****************************************************************"
 msg "installging codedeploy...."
 ## install codedeploy agent.
@@ -103,8 +89,6 @@ msg "codedeploy done.."
 
 warn "Step 8: Installing aws cli"
 warn "****************************************************************"
-warn "****************************************************************"
-warn "****************************************************************"
 msg "installing aws command..."
 ## install aws cli so we can install the .env file
 curl  "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscli.zip"
@@ -117,8 +101,6 @@ sudo ./aws/install
 
 warn "Step 9: Creating folder and setting up permission"
 warn "****************************************************************"
-warn "****************************************************************"
-warn "****************************************************************"
 msg "creating working folder..."
 ## clean up
 DIR="/home/ubuntu/api.mymango.video"
@@ -130,8 +112,6 @@ else
 fi
 
 warn "Step 10: Final setps"
-warn "****************************************************************"
-warn "****************************************************************"
 warn "****************************************************************"
 msg "updating the chown for the current details...."
 sudo chmod -R 0755 /home/ubuntu/api.mymango.video
